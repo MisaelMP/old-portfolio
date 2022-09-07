3cd82b53fcc7b84bad8190990282cf02a595ef48
Misael  M <misaelmdev@gmail.com>
2022-09-08 01:24:46 +1000

    added custom cursor

--- assets/js/main.js
+++ assets/js/main.js
@@ -44,4 +44,20 @@ $(document).ready(() => {
 		);
 		event.preventDefault();
 	});
+
+  /// custom cursor ///
+
+  const cursorRounded = document.querySelector('.rounded');
+	const cursorPointed = document.querySelector('.pointed');
+
+	const moveCursor = (e) => {
+		const mouseY = e.clientY;
+		const mouseX = e.clientX;
+
+		cursorRounded.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+
+		cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+	};
+
+	window.addEventListener('mousemove', moveCursor);
 });
