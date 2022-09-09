aff7bda8cb4939498a985cba3ed056c4e17dee41
Misael  M <misaelmdev@gmail.com>
2022-09-10 00:54:39 +1000

    added more changes to javascript, removing jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,64 +1,64 @@
 document.addEventListener('DOMContentLoaded', () => {
+	// variables //
+  const cursorCustom = document.querySelectorAll('[data-cursor-custom]');
+	const header = document.querySelector('[data-header]');
+	const navigationLink = document.querySelector('[data-nav-link]');
+	const mobileToggle = document.querySelector('[data-mobile-toggle]');
+
 	/// Diaginaol svg effect ///
 
 	window.onscroll = () => {
-		const scroll = $(window).scrollTop();
+		const scroll = window.scrollY;
 		//30 is the starting width
 		//alter the amount of growth by changing scroll/x
 		$('.diagonal-bg svg line').attr('stroke-width', 30 + scroll / 10 + '%');
 
-    // Sticky header //
-    if ($(window).scrollTop() > 100) {
-			$('.main_h').addClass('sticky');
+		// Sticky header //
+		if (scroll > 100) {
+			header.classList.add('is-sticky');
 		} else {
-			$('.main_h').removeClass('sticky');
+			header.classList.remove('is-sticky');
 		}
 	};
 
 	/// custom cursor ///
-	const cursorLarge = document.querySelector('.cursor-circle--large');
-	const cursorSmall = document.querySelector('.cursor-circle--small');
 
 	const moveCursor = (e) => {
 		const mouseY = e.pageY;
 		const mouseX = e.pageX;
 
-		cursorLarge.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-
-		cursorSmall.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+    for (let element of cursorCustom) {
+      element.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+    }
 	};
 
 	window.addEventListener('mousemove', moveCursor);
 
-	// Mobile Navigation
-	$('.mobile-toggle').click(function () {
-		if ($('.main_h').hasClass('open-nav')) {
-			$('.main_h').removeClass('open-nav');
+	// Mobile Navigation & Navigation Scroll //
+
+	mobileToggle.addEventListener('click', () => {
+		if (header.classList.contains('is-open')) {
+			header.classList.remove('is-open');
 		} else {
-			$('.main_h').addClass('open-nav');
+			header.classList.add('is-open');
 		}
 	});
 
-	$('.main_h li a').click(function () {
-		if ($('.main_h').hasClass('open-nav')) {
-			$('.navigation').removeClass('open-nav');
-			$('.main_h').removeClass('open-nav');
-		}
-	});
-
-	// navigation scroll
-	$('nav a').click(function (event) {
+	navigationLink.addEventListener('click', (e) => {
 		let id = $(this).attr('href');
 		let offset = 70;
 		let target = $(id).offset().top - offset;
+
+		if (header.classList.contains('is-open')) {
+			// $('.navigation').removeClass('is-open');
+			header.classList.remove('is-open');
+		}
 		$('html, body').animate(
 			{
 				scrollTop: target,
 			},
 			500
 		);
-		event.preventDefault();
+		e.preventDefault();
 	});
 });
-
- 
